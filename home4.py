f0fe43fb2b66b54923d9c9ac112ac2a2cc608025
DariaKutkanych <55439869+DariaKutkanych@users.noreply.github.com>
2019-09-26 14:49:42 +0300

    updated according to prev comments

--- home4.py
+++ home4.py
@@ -1,9 +1,9 @@
 # task 1.1 no replace
 def fin_list(line, number):
     word_list = ", ".join(x for x in line.split(", ") if int(x[:1:-4]) != number)
-    print (word_list)
+    return word_list
 
-fin_list("test1, test2, test3, test4, test5", int(input("Your number: ")))
+print(fin_list("test1, test2, test3, test4, test5", int(input("Your number: "))))
 
 # task 1.2 with replace
 def fin_list2(line, number):
@@ -13,8 +13,8 @@ def fin_list2(line, number):
         repl_line = ", " + "test" + number
     else:
         repl_line = "test" + number
-    print(line.replace(repl_line, "").replace(", ,",","))
-fin_list2("test1, test2, test3, test4, test5", input("Your number: "))
+    return line.replace(repl_line, "").replace(", ,",",")
+print(fin_list2("test1, test2, test3, test4, test5", input("Your number: ")))
 # task 2
 
 def domain(url):
@@ -22,8 +22,9 @@ def domain(url):
     clean_http = url[http_stat::]
     work_list = clean_http.split("/")
 
-    print(work_list[0].replace("www.", ""))
-domain("https://realpython.com/courses/python-thonny/")
+    return work_list[0].replace("www.", "")
+
+print(domain("https://realpython.com/courses/python-thonny/"))
 
 # task 3
 import random
@@ -31,8 +32,6 @@ import random
 rand_line = random.randint(0, 10)
 user_input = input("Please enter your number from 0 to 10: ")
 print(rand_line == user_input)
-
-
 
 
 
