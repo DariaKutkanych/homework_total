8fc8f658802ce4c3306fbff6679c3f6504dd191d
DariaKutkanych <d.goubenko@gmail.com>
2019-10-31 22:03:48 +0300

    Up to python 18

--- home17/warrior_test.py
+++ home17/warrior_test.py
@@ -10,6 +10,10 @@ class TestWarriors(unittest.TestCase):
         self.warrior1 = self.army1.train_swordsman("Peter", 100)
         self.warrior2 = self.army2.train_archer("Steve", 50)
 
+    def tearDown(self) -> None:
+        if self.warrior1 in self.army1.warriors:
+            self.army1.warriors.remove(self.warrior1)
+
     def test_warrior_damage_change(self):
         Warrior.hit(self.warrior2)
         Warrior.hit(self.warrior1)
@@ -33,3 +37,7 @@ class TestWarriors(unittest.TestCase):
 
         self.assertEqual(len(self.army2.warriors), 0)
         self.assertEqual(len(self.army1.warriors), 1)
+
+
+        #check if warrior is in warriors list: self.assertNotIn(self.warrior1, self.army1.warriors)
+        #check if warrior is dead: self.assertFalse(self.warrior1.is_alive)
