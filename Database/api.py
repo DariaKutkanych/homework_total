20a5a302ef6541e28f5b74f2c2c50691bc42ef0c
DariaKutkanych <55439869+DariaKutkanych@users.noreply.github.com>
2019-12-17 16:57:55 +0200

    Update api.py

--- Database/api.py
+++ Database/api.py
@@ -1,35 +1,30 @@
 import sqlite3
 
-def get_users():
+
+def get_data(table, data=None):
     with sqlite3.connect("untitled.db") as conn:
+        conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        users = cursor.execute('''select * from users''')
-        return users.fetchall()
+        query = '''select * from {}'''.format(table)
+        if data:
+            query = '''select * from Posts where user_id = {}'''.format(str(data))
+        users = cursor.execute(query)
+        result = users.fetchall()
+        k = []
 
-def post_users():
-    with sqlite3.connect("untitled.db") as conn:
-        cursor = conn.cursor()
-        users = cursor.execute('''insert into Users(?) values(?), **args''')
-        return users.fetchall()
+        for row in result:
+            k.append(dict(row))
+
+        return k
 
 
-# conn = sqlite3.connect("untitled.db")
-#
-# # with conn:
-# #     conn.execute('''insert into Users(username, email, password, age, city)
-# #      values("kevin", "s@kevin.com", "12345", "10", "Berdyansk")''')
-# # with conn:
-# #     conn.execute('''insert into Users(username, email, password, age, city)
-# #      values("phibi", "s@phibi.com", "12345", "60", "New York")''')
-#
-# with conn:
-#     curs = conn.cursor()
-#     # curs.execute('''select * from users where age between 25 and 60''')
-#     # curs.execute('''select * from users limit 2''')
-#     curs.execute('''select * from users''')
-#     # curs.execute('''delete from users where email like "%i.com"''')
-#     # curs.execute('''select * from users where email like "%.com"''')
-#     # curs.execute('''select * from users where city = "Berdyansk" or age > 25''')
-#     # curs.execute('''select * from users''')
-#     # curs.execute('''select distinct username, email, age, city from users''')
-#     print(curs.fetchall())
+def post_data(table, data):
+
+    v = tuple(data.values())
+    k = tuple(data.keys())
+
+    with sqlite3.connect('untitled.db') as conn:
+        conn.row_factory = sqlite3.Row
+        curs = conn.cursor()
+        query = '''insert into {} {} values {}'''.format(table, k, v)
+        curs.execute(query)
